a172ff58e18e12f81e1233e382da6f038c0e5365
Mitahy <71815931+Mitahhy@users.noreply.github.com>
2023-05-05 21:58:49 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{html,ts,tsx,js,jsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      signature: ["Great Vides"],
+    },
+  },
+  plugins: [],
+}
